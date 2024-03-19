b648f2ab3c8f19cfe1a106368b82d1f2154522ca
Hariom00027 <hariomsingh19092003@gmail.com>
2024-03-20 02:05:26 +0530

    Initial

---
+++ src/Area_triangle.java
@@ -0,0 +1,19 @@
+//Area Of Triangle
+
+import java.util.Scanner;
+
+public class Area_triangle {
+    public static void main(String[] args) {
+        Scanner n1= new Scanner(System.in);
+        System.out.println("Enter the Height of the triangle");
+        int H = n1.nextInt();
+
+        Scanner n2= new Scanner(System.in);
+        System.out.println("Enter the Height of the triangle");
+        int B = n2.nextInt();
+
+
+
+    }
+
+}
